b8b2decce0144114de44b84c591434b824963306
Pavl0ffN <pavl0ffn94@gmail.com>
2023-11-23 14:51:24 +0400

    add gsme logic

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ const game = {
   ctx: null,
   platform: null,
   ball: null,
+  running: true,
   blocks: [],
+  score: 0,
   rows: 4,
   cols: 8,
   with: 640,
@@ -28,7 +30,6 @@ const game = {
     window.addEventListener('keydown', (e) => {
       if (e.key === KEYS.SPACE) {
         this.platform.fire();
-        console.log('start');
       } else if (e.key === KEYS.LEFT || e.key === KEYS.RIGHT) {
         this.platform.start(e.key);
       }
@@ -39,7 +40,7 @@ const game = {
   },
   preload(callback) {
     let loaded = 0;
-    let required = Object.keys(this.sprites).length;
+    const required = Object.keys(this.sprites).length;
     let onImageLoad = () => {
       ++loaded;
       if (loaded >= required) {
@@ -54,24 +55,52 @@ const game = {
     }
   },
   update() {
+    this.collideBloks();
+    this.collidePlatform();
+    this.ball.collideWorldBounce();
+    this.platform.collideWorldPlatform();
     this.platform.move();
     this.ball.move();
-
-    for (let block of this.blocks) {
-      this.ball.collide(block);
+  },
+  end(text) {
+    this.running = false;
+    confirm(text);
+    window.location.reload();
+  },
+  addScore() {
+    ++this.score;
+    if (this.score === this.blocks.length) {
+      game.end('You Win');
+    }
+  },
+  collideBloks() {
+    this.blocks.forEach((block) => {
+      if (block.active && this.ball.collide(block)) {
+        this.ball.bumpBlock(block);
+      }
+    });
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
     }
   },
   run() {
-    window.requestAnimationFrame(() => {
-      this.update();
-      this.render();
-      this.run();
-    });
+    if (this.running) {
+      window.requestAnimationFrame(() => {
+        this.update();
+        this.render();
+        this.run();
+      });
+    }
   },
   create() {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          active: true,
+          with: 60,
+          height: 20,
           x: 64 * col + 65,
           y: 24 * row + 35,
         });
@@ -97,9 +126,11 @@ const game = {
     this.renderBlocks();
   },
   renderBlocks() {
-    for (let block of this.blocks) {
-      this.ctx.drawImage(this.sprites.block, block.x, block.y);
-    }
+    this.blocks.forEach((block) => {
+      if (block.active) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }
+    });
   },
   start: function () {
     this.init();
@@ -133,7 +164,61 @@ game.ball = {
       this.x += this.dx;
     }
   },
-  collaide(element) {},
+  collide(element) {
+    const x = this.x + this.dx;
+    const y = this.y + this.dy;
+    if (
+      x + this.with > element.x &&
+      x < element.x + element.with &&
+      y + this.height > element.y &&
+      y < element.y + element.height
+    ) {
+      return true;
+    }
+    return false;
+  },
+  collideWorldBounce() {
+    const x = this.x + this.dx;
+    const y = this.y + this.dy;
+
+    const ballLeft = x;
+    const ballRight = ballLeft + this.with;
+    const ballTop = y;
+    const bollBottom = ballTop + this.height;
+
+    const worldLeft = 0;
+    const worldRight = game.with;
+    const worldTop = 0;
+    const worldBotttom = game.height;
+
+    if (ballLeft < worldLeft) {
+      this.x = 0;
+      this.dx = this.velocity;
+    } else if (ballRight > worldRight) {
+      this.x = worldRight - this.with;
+      this.dx = -this.velocity;
+    } else if (ballTop < worldTop) {
+      this.y = 0;
+      this.dy = this.velocity;
+    } else if (bollBottom > worldBotttom) {
+      game.end('You lose');
+    }
+  },
+  bumpBlock(block) {
+    this.dy *= -1;
+    block.active = false;
+    game.addScore();
+  },
+  bumpPlatform(platform) {
+    if (platform.dx) {
+      this.x += platform.dx;
+    }
+    if (this.dy > 0) {
+      const touchX = this.x + this.with / 2;
+      this.dy = -this.velocity;
+      this.dx = this.velocity * platform.getTouchOffet(touchX);
+    }
+  },
 };
 
 game.platform = {
@@ -141,6 +226,8 @@ game.platform = {
   dx: 0,
   x: 280,
   y: 300,
+  with: 100,
+  height: 14,
   ball: game.ball,
   fire() {
     if (this.ball) {
@@ -164,6 +251,23 @@ game.platform = {
       if (this.ball) {
         this.ball.x += this.dx;
       }
+    }
+  },
+  getTouchOffet(x) {
+    const diff = this.x + this.with - x;
+    const offset = this.with - diff;
+    const result = (2 * offset) / this.with;
+    return result - 1;
+  },
+  collideWorldPlatform() {
+    const x = this.x + this.dx;
+    const platformLeft = x;
+    const platformRight = platformLeft + this.with;
+    const worldLeft = 0;
+    const worldRight = game.with;
+
+    if (platformLeft < worldLeft || platformRight > worldRight) {
+      this.dx = 0;
     }
   },
 };
